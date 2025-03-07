e12a3780b635d61d9b631dc770dab05de8c5bc66
Héctor Salas <hector.salas@emd.io>
2025-03-07 15:47:39 -0300

    Update logo web

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { Button, Container, Flex, HStack, Text, useColorMode } from '@chakra-ui/react'
+import { Button, Container, Flex, HStack, Image, Text, useColorMode } from '@chakra-ui/react'
 import { PlusSquareIcon } from '@chakra-ui/icons'
 import { IoMoon } from 'react-icons/io5'
 import { LuSun } from 'react-icons/lu'
+import logo from '../../public/logo_onlinestore.svg';
 
 const Navbar = () => {
     const { colorMode, toggleColorMode } = useColorMode();
@@ -18,6 +19,7 @@ const Navbar = () => {
                     sm: "row"
                 }}
             >
+                <Image src={logo}  width={100}/>
                 <Text
                     fontSize={{ base: "22", sm: "28" }}
                     fontWeight={"bold"}
@@ -26,7 +28,7 @@ const Navbar = () => {
                     bgGradient={"linear(to-r, cyan.400, blue.500)"}
                     bgClip={"text"}
                 >
-                    <Link to={"/"}>Product Store 🛒 </Link>
+                    <Link to={"/"} style={{textTransform: "capitalize"}}>Welcome to OnlineStore 🛒 </Link>
                 </Text>
 
                 <HStack spacing={2} alignItems={"center"}>
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -16,13 +16,13 @@ const HomePage = () => {
     <Container maxW='container.xl' py={12}>
       <VStack spacing={8}>
         <Text
-          fontSize={"30"}
+          fontSize={"25"}
           fontWeight={"bold"}
           bgGradient={"linear(to-r, cyan.400, blue.500)"}
           bgClip={"text"}
           textAlign={"center"}
         >
-          Current Products 🎒
+          Current Products 
         </Text>
 
         <SimpleGrid
